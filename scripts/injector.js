2a84217cb5db45f1e324de3259f6e97d14ac798b
mys <q1311588796@gmail.com>
2025-12-30 17:05:39 +0800

    add icon

---
+++ scripts/injector.js
@@ -0,0 +1,3 @@
+// hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="/css/custom.css">', 'default');
+// scripts/inject-link.js
+hexo.extend.injector.register('body_end', '<script src="/js/custom-footer.js"></script>', 'default');
---
+++ source/js/custom-footer.js
@@ -0,0 +1,21 @@
+// source/js/custom-footer.js
+
+function changeFooterContent() {
+  // 1. 选中页脚的 P 标签
+  var target = document.querySelector(".level > .level-start > p");
+
+  // 2. 检查元素是否存在
+  if (target) {
+      // 关键在这里：href="mailto:你的邮箱"
+        // var emailLink = '<a href="mailto:1311588796@qq.com">给我发邮件</a>';
+        
+        // 拼接内容： © 2024 名字 | 给我发邮件
+        target.innerHTML = '© 2025 ZeroShiro | Thanks for reading.';
+      // 修改样式
+      // target.style.color = 'red';
+  }
+}
+
+// 4. 监听事件 (兼容 PJAX)
+document.addEventListener("DOMContentLoaded", changeFooterContent);
+document.addEventListener("pjax:complete", changeFooterContent);
